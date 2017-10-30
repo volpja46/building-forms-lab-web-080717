a5f61892fb3501181c6706da5a65a0d404bdc1c1
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-30 12:50:15 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,87 @@
+// import React, { Component } from 'react';
+//
+// class BandInput extends Component {
+//
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       text: '',
+//     }
+//   }
+//
+//   handleChange = (event) => {
+//     // change state on keydown event
+//     this.setState = ({
+//       text: event.target.value
+//     });
+//   }
+//
+//   handleSubmit = (event) => {
+//     event.preventDefault();
+//     this.props.store.dispatch({
+//       type: 'ADD_BAND',
+//       band: this.state.text
+//     });
+//     this.setState({
+//       text: '',
+//     });
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <form onSubmit={(event) => this.handleSubmit(event)}>
+//           <input
+//             type="text"
+//             value={this.state.text}
+//             onChange={(event) => this.handleChange(event)} />
+//           <input type="submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+//
+// export default BandInput;
+
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text
+    });
+    this.setState({
+      text: '',
+    });
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
